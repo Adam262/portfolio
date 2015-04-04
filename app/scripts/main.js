91995b06f041daf136bed616a9fea9f7cd0b6955
Adam262 <abarcan@gmail.com>
2015-04-04 11:11:18 -0400

    Add content to portfolio carosel

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,25 @@
 $(document).ready(function(){
+  "use strict";
+
   $('#pagepiling').pagepiling({
-      
-      loopBottom: true,
-      loopTop: true,
-      sectionsColor: ['#8FB7D6', '#FFFFFF', '#519E8A', '#808F85', '#0F1108'],
+
+    loopBottom: true,
+    loopTop: true,
+    sectionsColor: ['#8FB7D6', '#FFFFFF', '#519E8A', '#808F85', '#0F1108']
   
     });
-})
+
+  $('.slider').bxSlider({
+
+    ticker: true,
+    tickerHover: true,
+    adaptiveHeight: true,
+    minSlides: 2,
+    maxSlides: 2,
+    speed: 25000
+
+  });
+});
 
 // add mailto to Get in Touch botton
 function getInTouch(){
