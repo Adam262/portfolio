481cb233b9b52d7d7741f08755113c14fac781a9
Adam262 <abarcan@gmail.com>
2015-06-04 00:23:51 -0400

    Make toggle slides for portfolio. Uninstall slider library

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,20 +9,21 @@ $(document).ready(function(){
   
     });
 
-  $('.slider').bxSlider({
+  $('.slide-image').click(function() {
+      $(this).slideToggle(1000);
+      $(this).parent().children('.slide-text').slideToggle(1000);
+    })
+ 
+   $('.slide-text').click(function() {
+      $(this).slideToggle(1000);
+      $(this).parent().children('.slide-image').slideToggle(1000);
+    })
 
-    ticker: true,
-    tickerHover: true,
-    adaptiveHeight: true,
-    minSlides: 2,
-    maxSlides: 2,
-    speed: 25000
-
-  });
 });
 
 // add mailto to Get in Touch botton
 function getInTouch(){
+  'use strict';
   var email = "abarcan@gmail.com";
   var subject = "Hello from your site!";
   location.href = "mailto:"+email+"?"+"subject="+subject;
