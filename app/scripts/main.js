614829af93ea623bdb53f225b1dc38ea3d19c0d6
Adam262 <abarcan@gmail.com>
2015-07-14 23:22:20 -0400

    Refctor to module pattern

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -20,66 +20,71 @@ var Config = {
   }
 };
 
-var Portfolio = {   
-  configurePagepiling: function() {
-    $pagepiling.pagepiling({
-      loopBottom: true,
-      loopTop: true,
-      sectionsColor: Config.pagePiling.colors
-    });
-  },
-
-  slideImage: function() {
+var Portfolio = (
+  function() { 
     var slideToggle = Config.slideToggle;
 
-    $slideImage.click(function() {
-      $(this).slideToggle(slideToggle.up);
-      $(this).parent().children('.slide-text').slideToggle(slideToggle.down);
-    });
-  },
+    function configurePagepiling() {  
+      $pagepiling.pagepiling({
+        loopBottom: true,
+        loopTop: true,
+        sectionsColor: Config.pagePiling.colors
+      });
+    }
 
-  slideText: function() {
-    var slideToggle = Config.slideToggle;
+    function slideImage() {
+      $slideImage.click(function() {
+        console.log($(this))
+        $(this).slideToggle(slideToggle.up);
+        $(this).parent().children('.slide-text').slideToggle(slideToggle.down);
+      });
+    }
 
-    $slideText.click(function() {
-      $(this).slideToggle(slideToggle.up);
-      $(this).parent().children('.slide-image').slideToggle(slideToggle.down);
-    });
-  },
+    function slideText() {
+      $slideText.click(function() {
+        $(this).slideToggle(slideToggle.up);
+        $(this).parent().children('.slide-image').slideToggle(slideToggle.down);
+      });
+    }
 
-  landingTitle: function() {
-    var fadeInterval = Config.landingTitle.fadeInterval,
-      fadeIn = Config.landingTitle.fadeIn,
-      fadeOut = Config.landingTitle.fadeOut;
+    function landingTitle() {
+      var fadeInterval = Config.landingTitle.fadeInterval,
+        fadeIn = Config.landingTitle.fadeIn,
+        fadeOut = Config.landingTitle.fadeOut;
 
-    setInterval(function() {
-      $landingTitle.fadeTo(fadeInterval, fadeOut, function() {
-        $(this).html('Automation Engineer').fadeTo(fadeInterval, fadeIn, function() {
-          $(this).html('Photog').fadeTo(fadeInterval, fadeOut, function() {
-            $(this).html('Fullstack Web Developer').fadeTo(fadeInterval, fadeIn)
+      setInterval(function() {
+        $landingTitle.fadeTo(fadeInterval, fadeOut, function() {
+          $(this).html('Automation Engineer').fadeTo(fadeInterval, fadeIn, function() {
+            $(this).html('Photog').fadeTo(fadeInterval, fadeOut, function() {
+              $(this).html('Fullstack Web Developer').fadeTo(fadeInterval, fadeIn)
+            })
           })
         })
-      })
-    }, 4000)
-  },
+      }, 4000)
+    }
 
-  getInTouch: function() {
-    var email   = "abarcan@gmail.com",
-        subject = "Hello from your site!";
+    function getInTouch() {
+      var email   = "abarcan@gmail.com",
+          subject = "Hello from your site!";
 
-    location.href = "mailto:"+email+"?"+"subject="+subject;
-  },
+      location.href = "mailto:"+email+"?"+"subject="+subject;
+    }
 
-  init: function() {
-    this.configurePagepiling();
-    this.slideImage();
-    this.slideText();
-    this.landingTitle();
-  }
-};
+    function registerEvents() {
+      $('#btn-contact').on('click', getInTouch)
+    }
 
-$(document).ready(function() {
-  "use strict";
+    function init() {
+      registerEvents();
+      configurePagepiling();
+      slideImage();
+      slideText();
+      landingTitle();
+    }
 
-  Portfolio.init();
-});
+    return { init: init };
+
+  }()
+);
+
+$(document).ready(Portfolio.init);
